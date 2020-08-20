cf615cd5b4ba07d0e36bc42aad3558ea36c71fbb
JB_Anonyme <uchallengeme007@gmail.com>
2020-08-20 18:00:29 +0200

    Update

---
+++ precache-manifest.28e0302b72362b55837b19744434143c.js
@@ -0,0 +1,106 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "55fc6ca73098f7c6d22c9eaab4638e48",
+    "url": "/index.html"
+  },
+  {
+    "revision": "63f0a83ded7f07660796",
+    "url": "/static/css/main.35e18127.chunk.css"
+  },
+  {
+    "revision": "bb118fb52a7f92104583",
+    "url": "/static/js/2.ecc69bc5.chunk.js"
+  },
+  {
+    "revision": "4214db9a6c23cd334176e62fe82579f5",
+    "url": "/static/js/2.ecc69bc5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "63f0a83ded7f07660796",
+    "url": "/static/js/main.da03f4a7.chunk.js"
+  },
+  {
+    "revision": "32d90a7c70bb327203a7",
+    "url": "/static/js/runtime-main.f9b6aba0.js"
+  },
+  {
+    "revision": "1eafbd9e4e386a13881efb7aa790a71c",
+    "url": "/static/media/Android_Card.1eafbd9e.jpg"
+  },
+  {
+    "revision": "e4fdc3b4159bb1128410a6b7f7f474b9",
+    "url": "/static/media/Android_Icon.e4fdc3b4.png"
+  },
+  {
+    "revision": "d9e54384f1b13100be01a58c6e50ea81",
+    "url": "/static/media/Apache-Derby_Icon.d9e54384.png"
+  },
+  {
+    "revision": "7230beb2776d1347e72c9ec32f56ba98",
+    "url": "/static/media/Avatar.7230beb2.png"
+  },
+  {
+    "revision": "a7d6a458ae79fd383af0f7b8eb1b910b",
+    "url": "/static/media/Back-end_Icon.a7d6a458.ico"
+  },
+  {
+    "revision": "12aadcad162c2ce0b7d5bc9f60e60c66",
+    "url": "/static/media/Bootsfaces_Icon.12aadcad.png"
+  },
+  {
+    "revision": "2d393eace2aad2a62a6d143227930e42",
+    "url": "/static/media/CCNA-RS_Icon.2d393eac.png"
+  },
+  {
+    "revision": "ab7da72ab46e1af73d2d425164b0e1ef",
+    "url": "/static/media/CSS_Icon.ab7da72a.png"
+  },
+  {
+    "revision": "b9311c69ffa934bb4e8e45fbf1b744ea",
+    "url": "/static/media/Front-end_Icon.b9311c69.ico"
+  },
+  {
+    "revision": "23e43821b9db83e8d8d150775a46100e",
+    "url": "/static/media/HTML_Icon.23e43821.png"
+  },
+  {
+    "revision": "117f86f3d3ed2f17f641c65583375182",
+    "url": "/static/media/IZ-Logo.117f86f3.png"
+  },
+  {
+    "revision": "06d4f86c5ad6881886f7d69abf8a249e",
+    "url": "/static/media/JS_Icon.06d4f86c.png"
+  },
+  {
+    "revision": "839555e8c0de67875861c59ccbbea7c5",
+    "url": "/static/media/Java_Icon.839555e8.png"
+  },
+  {
+    "revision": "d2b1e19091a3530dfb8bec06b5a926dc",
+    "url": "/static/media/Material_Icon.d2b1e190.png"
+  },
+  {
+    "revision": "20b30f18748a781a04c1430010c64190",
+    "url": "/static/media/MySQL_Icon.20b30f18.png"
+  },
+  {
+    "revision": "e45950de65ad036654e2c92fb62b0b21",
+    "url": "/static/media/Net_Card.e45950de.png"
+  },
+  {
+    "revision": "7474bc10bbbfc26365d9e17f0da14e98",
+    "url": "/static/media/Primefaces_Icon.7474bc10.png"
+  },
+  {
+    "revision": "cb9c6bd221a090272db21a2e02cd48c0",
+    "url": "/static/media/Soft_Card.cb9c6bd2.jpeg"
+  },
+  {
+    "revision": "d89dbebc11d3c2797ac187ee7b249208",
+    "url": "/static/media/contact-us.d89dbebc.svg"
+  },
+  {
+    "revision": "b58c90c33c744ea154553ba1bb02fb27",
+    "url": "/static/media/home-header-dark.b58c90c3.jpg"
+  }
+]);
